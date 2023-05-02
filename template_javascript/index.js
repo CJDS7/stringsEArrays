4c63bb2f5fb08f2a6e3c3f889bfd0c2cfc6ef93e
cristianpandrade <cristianeduardfarias@example.com>
2023-05-02 20:15:03 -0300

    Primeiro commit!

---
+++ template_javascript/index.js
@@ -0,0 +1,48 @@
+// Exercício de escrita de código 1
+
+// const userEmail = prompt("Digite seu e-mail");
+// const userName = prompt("Digite seu nome completo:");
+
+// console.log(
+//   `O e-mail ${userEmail} foi cadastrado com sucesso. Seja bem-vindo(a), ${userName}!`
+// );
+
+// Exercício de escrita de código 2
+
+// const favoriteFood = ["Pizza", "Chocolate", "Refri", "Bolo", "Torta"];
+
+// console.log(favoriteFood);
+// console.log(
+//   "Essas são as minhas comidas preferidas: " +
+//     "\n" +
+//     favoriteFood[0] +
+//     "\n" +
+//     favoriteFood[1] +
+//     "\n" +
+//     favoriteFood[2] +
+//     "\n" +
+//     favoriteFood[3] +
+//     "\n" +
+//     favoriteFood[4]
+// );
+
+// Exercício de escrita de código 3
+
+// const emptyArray = [];
+
+// emptyArray.push(prompt("Escreva alguma tarefa que você tenha que fazer:"));
+// emptyArray.push(prompt("Escreva outra tarefa que você tenha que fazer:"));
+// emptyArray.push(prompt("Escreve a última tarefa que você tem que fazer:"));
+
+// console.log(emptyArray);
+
+// emptyArray.splice(
+//   Number(
+//     prompt(
+//       "Digite o índice de uma atividade que você já terminou, de 0, 1 ou 2:"
+//     )
+//   ),
+//   1
+// );
+
+// console.log(emptyArray);
